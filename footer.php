78032910f9557d1859b84ab75b69b8a4afbbe2bd
Bustin Jeiber <xtrimbiz@yahoo.com>
2014-04-19 17:44:08 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+<footer style="position:fixed;bottom:0px;width:100%;text-align:center"><hr/>
+<h6>Powered By &copy
+<span class="label label-default">relationshipwithmycomputer.com</span></h6>
+</footer>
+
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
